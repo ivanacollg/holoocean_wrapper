a1fa5e314cf9a10e1f40754e32735251a9c4f6f2
ivanacollg <ivanacollado@gmail.com>
2024-03-14 11:14:34 -0400

    Updated example

--- scripts/example.py
+++ scripts/example.py
@@ -1,11 +1,60 @@
+#!/usr/bin/env python3
+
 import holoocean
 import numpy as np
+from pynput import keyboard
 
-env = holoocean.make("PierHarbor-HoveringImagingSonar")
+pressed_keys = list()
+force = 55
 
-# The hovering AUV takes a command for each thruster
-command = np.array([10,10,10,10,0,0,0,0])
+def on_press(key):
+    global pressed_keys
+    if hasattr(key, 'char'):
+        pressed_keys.append(key.char)
+        pressed_keys = list(set(pressed_keys))
 
-#for _ in range(180):
-while True:
-   state = env.step(command)
+def on_release(key):
+    global pressed_keys
+    if hasattr(key, 'char'):
+        pressed_keys.remove(key.char)
+
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+def parse_keys(keys, val):
+    command = np.zeros(8)
+    if 'i' in keys:
+        command[0:4] += val
+    if 'k' in keys:
+        command[0:4] -= val
+    if 'j' in keys:
+        command[[4,7]] += val
+        command[[5,6]] -= val
+    if 'l' in keys:
+        command[[4,7]] -= val
+        command[[5,6]] += val
+
+    if 'w' in keys:
+        command[4:8] += val
+    if 's' in keys:
+        command[4:8] -= val
+    if 'a' in keys:
+        command[[4,6]] += val
+        command[[5,7]] -= val
+    if 'd' in keys:
+        command[[4,6]] -= val
+        command[[5,7]] += val
+
+    return command
+
+with holoocean.make("OpenWater-Hovering") as env:
+    while True:
+        if 'q' in pressed_keys:
+            break
+        command = parse_keys(pressed_keys, force)
+
+        #send to holoocean
+        env.act("auv0", command)
+        state = env.tick()
